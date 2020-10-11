ef3f9f9038f0892f1267f804b2c58d4de64e3c64
Caroline <caroline@otmo7.com>
2020-10-10 21:42:25 -0400

    update variable name

--- NomeSobrenome.py
+++ NomeSobrenome.py
@@ -1,5 +1,5 @@
 nome = input("Qual e o seu nome? ")
-sobrenomeDaMae = input("Qual o sobrenome da sua mae? ")
-sobrenomeDoPai = input("Qual o sobrenome do seu pai? ")
+sobrenomeMae = input("Qual o sobrenome da sua mae? ")
+sobrenomePai = input("Qual o sobrenome do seu pai? ")
 
-print("Bom dia,", nome, sobrenomeDaMae, sobrenomeDoPai, ". Seja muito bem vindo ao nosso grupo!")
+print("Bom dia,", nome, sobrenomeMae, sobrenomePai, ". Seja muito bem vindo ao nosso grupo!")
