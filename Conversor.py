1b412789801c34e185e962729b86a96d5d597125
Caroline <caroline@otmo7.com>
2020-10-10 21:11:58 -0400

    first commit

---
+++ Conversor.py
@@ -0,0 +1,6 @@
+temperaturaFahrenheit = input ("Digite uma temperatura em Faherenheit:  ")
+
+temperaturaCelsius = (float(temperaturaFahrenheit)-32)*5//9
+
+print("A temperatura em celsius e ", temperaturaCelsius)
+
