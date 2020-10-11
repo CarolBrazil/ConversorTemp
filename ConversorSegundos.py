e5cb3e60e4bf9c14f3b7aa2d7c82056cb941595b
Caroline <caroline@otmo7.com>
2020-10-10 23:01:25 -0400

    update dias

--- ConversorSegundos.py
+++ ConversorSegundos.py
@@ -1,8 +1,10 @@
 segundos_str = input("Por favor, entre com o numero de segundos que deseja converter: ")
 total_segs = int(segundos_str)
-horas = total_segs // 3600
-segs_restantes = total_segs % 3600
-minutos = segs_restantes // 60
-segs_restantes_final = segs_restantes % 60
+dias = total_segs // 86400
+segs_restantes = total_segs % 86400
+horas = segs_restantes // 3600
+segs_restantes1 = segs_restantes % 3600
+minutos = segs_restantes1 // 60
+segs_restantes_final = segs_restantes1 % 60
 
-print (horas, "Horas, ", minutos, "Minutos e ", segs_restantes_final, "Segundos.")
+print (dias, "Dias,", horas, "Horas, ", minutos, "Minutos e ", segs_restantes_final, "Segundos.")
